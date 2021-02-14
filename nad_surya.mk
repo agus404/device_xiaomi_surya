a0dc6acd4057736d08ea14935c1de9a678c18a0c
agustkid <agustrikusuma003@gmail.com>
2021-02-15 03:14:05 +0700

    surya: Wellcome to Nusantara
    Signed-off-by: agustkid <agustkid@gmail.com>

--- nad_surya.mk
+++ nad_surya.mk
@@ -6,18 +6,19 @@
 
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# ANX Camera
-$(call inherit-product, vendor/ANXCamera/config.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 
-# Boot animation
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_surya
+PRODUCT_NAME := nad_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3
